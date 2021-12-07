bc20ef4fe2bc77348aa868f259d55f828cadbfc2
fridajac <frida_jacobsson@live.se>
2021-12-07 15:16:32 +0100

    Smaller changes in fetchTracks.js, its now working

--- fetchTracks.js
+++ fetchTracks.js
@@ -1,8 +1,7 @@
 import { OAuthToken, clientSecret, clientID } from "./spotifyPasswordAndKeys.js";
-import fetch from 'node-fetch';
 import request from 'request';
 
-function test() {
+function fetchResults() {
     var authOptions = {
         url: 'https://accounts.spotify.com/api/token',
         headers: {
@@ -17,23 +16,21 @@ function test() {
     request.post(authOptions, function(error, response, body) {
         if (!error && response.statusCode === 200) {
             var token = body.access_token;
-            return token;
-        }
-    });
-}
-async function fetchResults() {
 
-    const response = await fetch('https://api.spotify.com/v1/search?q=remaster%2520track%3ADoxy%2Bartist%3AMiles%2520Davis&type=track%2Cartist&market=ES&limit=10&offset=5', {
-        method: 'GET',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + test(),
+            var options = {
+                url: 'https://api.spotify.com/v1/search?q=remaster%2520track%3ADoxy%2Bartist%3AMiles%2520Davis&type=track%2Cartist&market=ES&limit=10&offset=5',
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + token
+                },
+                json: true
+            };
+            request.get(options, function(error, response, body) {
+                console.log(body);
+            });
         }
-        .then(response => response.json())
-        .then(function(data) {
-            console.log(data);
-        })
     });
 }
 fetchResults();
