ff81e45d8356a1e71861df1d728a23d74c149dfc
fridajac <frida_jacobsson@live.se>
2021-12-07 13:25:43 +0100

    Smaller changes with export, added package.json files

--- fetchSongs.js
+++ fetchSongs.js
@@ -1,9 +1,10 @@
-import { OAuthToken } from "spotifyPasswordsAndKeys.js";
+import { OAuthToken } from "./spotifyPasswordAndKeys.js";
+import fetch from 'node-fetch';
 
 fetchResults();
 
 async function fetchResults() {
-    const response = await fetch('https://api.spotify.com/v1/search?q=searchword&type=track&limit=10', {
+    const response = await fetch('https://api.spotify.com/v1/search?q=remaster%2520track%3ADoxy%2Bartist%3AMiles%2520Davis&type=track%2Cartist&market=ES&limit=10&offset=5', {
         method: 'GET',
         headers: {
             'Accept': 'application/json',
